c6ea388dbe1a147698bd29d38c542ef2e510b686
Matthew Ault <matthew.ault@gmail.com>
2017-08-20 18:59:08 +0100

    get_data: initial create table sql

--- get_data.py
+++ get_data.py
@@ -6,6 +6,7 @@ import argparse
 import boto3
 import gzip
 import os
+import psycopg2
 
 parser = argparse.ArgumentParser(description='IMDB data updater script')
 parser.add_argument('--do-get', dest='do_get', action='store_true')
@@ -36,4 +37,49 @@ if args.do_get:
 
 # create tables
 
-# insert data into tables
+# first do:
+# create_db aulty
+try:
+	connect_str = "dbname='aulty' user='aulty'"
+	# use our connection values to establish a connection
+	conn = psycopg2.connect(connect_str)
+	# create a psycopg2 cursor that can execute queries
+	cursor = conn.cursor()
+	# create a new table with a single column called "name"
+
+
+	# create tables
+	cursor.execute("""CREATE TABLE "title_principals" (
+	tconst          text primary key,
+	principalcast   text[]
+	);""")
+
+
+	# insert data into tables
+
+	# TODO as a generator?
+	data = []
+	with open("title.principals.tsv") as f:
+		# TODO as list comprehension for efficiency?
+		# data = [line.rstrip("\n").split("\t") for line in f.readlines()]
+
+		line_no = 0 # skip header
+		for line in f.readlines():
+			if line_no != 0:
+				tconst, nconsts = line.rstrip("\n").split("\t")
+				s = """INSERT INTO "title_principals"(tconst, principalcast) VALUES ('%s', '{%s}');""" % (tconst, nconsts)
+				cursor.execute(s)
+				#print(s)
+			if line_no%10000==0:
+				print(line_no)
+			line_no+=1
+	conn.commit()
+
+	# as a test
+	cursor.execute("""SELECT * FROM title_principals WHERE tconst = 'tt0000001';""")
+	rows = cursor.fetchall()
+	print(rows)
+
+except Exception as e:
+	print("Uh oh, can't connect. Invalid dbname, user or password?")
+	print(e)
