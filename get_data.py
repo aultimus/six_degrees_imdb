267a68f6ab17009fa354d19f7442433cc1358a03
Matthew Ault <matthew.ault@gmail.com>
2017-08-20 20:19:32 +0100

    get_data.py: parse title.basics to db

--- get_data.py
+++ get_data.py
@@ -7,9 +7,11 @@ import boto3
 import gzip
 import os
 import psycopg2
+import traceback
 
 parser = argparse.ArgumentParser(description='IMDB data updater script')
 parser.add_argument('--do-get', dest='do_get', action='store_true')
+parser.add_argument('--test-data', dest='test_data', action='store_true')
 args = parser.parse_args()
 
 if args.do_get:
@@ -35,7 +37,72 @@ if args.do_get:
 
 # do_get == false assumes unarchived data files exist
 
-# create tables
+def replace_null(s):
+	return s.replace(r"\N", "0")
+
+def db_title_principals(cursor, f_name):
+	cursor.execute("""CREATE TABLE "title_principals" (
+	tconst          text primary key,
+	principalcast   text[]
+	);""")
+
+	with open(f_name) as f:
+	# TODO as list comprehension for efficiency?
+	# data = [line.rstrip("\n").split("\t") for line in f.readlines()]
+		line_no = 0 # skip header
+		for line in f.readlines():
+			if line_no != 0:
+				tconst, nconsts = line.rstrip("\n").split("\t")
+				s = """INSERT INTO "title_principals"(tconst, principalcast) VALUES ('%s', '{%s}');""" % (tconst, nconsts)
+				cursor.execute(s)
+				#print(s)
+			if line_no%10000==0:
+				print(line_no)
+			line_no+=1
+		conn.commit()
+
+	# as a test
+	cursor.execute("""SELECT * FROM title_principals WHERE tconst = 'tt0000001';""")
+	rows = cursor.fetchall()
+	print(rows)
+
+def db_title_basics(cursor, f_name):
+	cursor.execute("""CREATE TABLE "title_basics" (
+	tconst				text primary key,
+	titleType			text,
+	primaryTitle		text,
+	originalTitle		text,
+	isAdult				int,
+	startYear			int,
+	endYear				int,
+	runtimeMinutes		int,
+	genres				text[]
+	);""")
+
+	with open(f_name) as f:
+		line_no = 0 # skip header
+		for line in f.readlines():
+			if line_no != 0:
+				line = replace_null(line)
+				others, genres = line.rsplit("\t", 1)
+				genres = "{" + genres.rstrip("\n") + "}"
+				others = others.split("\t")
+				others = str(others).lstrip("[").rstrip("]")
+				print(genres)
+				print(others)
+				
+				s = """INSERT INTO "title_basics"(tconst, titleType, primaryTitle, originalTitle, isAdult, startYear, endYear, runtimeMinutes, genres) VALUES (%s,'%s');""" % (others, genres)
+				#print(s)
+				cursor.execute(s)
+			if line_no%10000==0:
+				print(line_no)
+			line_no+=1
+	conn.commit()
+
+	# as a test
+	cursor.execute("""SELECT * FROM title_basics WHERE tconst = 'tt0000009';""")
+	rows = cursor.fetchall()
+	print(rows)
 
 # first do:
 # create_db aulty
@@ -47,39 +114,15 @@ try:
 	cursor = conn.cursor()
 	# create a new table with a single column called "name"
 
+	test_str = ""
+	if args.test_data:
+		test_str = ".test"
 
-	# create tables
-	cursor.execute("""CREATE TABLE "title_principals" (
-	tconst          text primary key,
-	principalcast   text[]
-	);""")
+	#db_title_principals(cursor, "title.principals" + test_str + ".tsv")
+	db_title_basics(cursor, "title.basics" + test_str + ".tsv")
 
-
-	# insert data into tables
-
-	# TODO as a generator?
-	data = []
-	with open("title.principals.tsv") as f:
-		# TODO as list comprehension for efficiency?
-		# data = [line.rstrip("\n").split("\t") for line in f.readlines()]
-
-		line_no = 0 # skip header
-		for line in f.readlines():
-			if line_no != 0:
-				tconst, nconsts = line.rstrip("\n").split("\t")
-				s = """INSERT INTO "title_principals"(tconst, principalcast) VALUES ('%s', '{%s}');""" % (tconst, nconsts)
-				cursor.execute(s)
-				#print(s)
-			if line_no%10000==0:
-				print(line_no)
-			line_no+=1
-	conn.commit()
-
-	# as a test
-	cursor.execute("""SELECT * FROM title_principals WHERE tconst = 'tt0000001';""")
-	rows = cursor.fetchall()
-	print(rows)
 
 except Exception as e:
 	print("Uh oh, can't connect. Invalid dbname, user or password?")
 	print(e)
+	traceback.print_exc()
