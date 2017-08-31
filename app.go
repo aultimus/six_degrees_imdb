179c4e854fcfc30c169101cb56e6cd8b56213e09
Matthew Ault <matthew.ault@gmail.com>
2017-08-31 17:43:36 +0100

    app.go: do name->nconst lookup.

--- app.go
+++ app.go
@@ -53,11 +53,19 @@ func (a *App) Init() error {
 	if err != nil {
 		return err
 	}
+	a.db = db
+
 	return nil
 }
 
 func (a *App) Run() error {
 	return a.server.ListenAndServe()
+}
+
+// TODO: batching sql ops?
+func nconstsForName(db *sql.DB, name string) (*sql.Rows, error) {
+	rows, err := db.Query("SELECT * FROM name_basics WHERE primaryname = $1", name)
+	return rows, err
 }
 
 // PathBetweenHandler for the given two actors attempts to find the shortest
@@ -71,14 +79,26 @@ func (a *App) PathBetweenHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Calculating path between %s and %s\n", actorA, actorB)
 
 	// lookup nconst for actors
+	possibleA, err := nconstsForName(a.db, actorA)
+	// TODO: some better error handling / response for user's sake
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	possibleB, err := nconstsForName(a.db, actorB)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	spew.Dump(possibleA)
+	spew.Dump(possibleB)
+
+	// we need to provide a mechansim to resolve ambiguity via the frontend
+	// Say: There are x people with name y in the database
+	// Please choose one
 
 	// do BFS
 
 	w.Write([]byte("foo"))
 }
-
-//rows, err := db.Query("SELECT * FROM title_principals")
-//if err != nil {
-//	log.Fatal(err)
-//}
-//spew.Dump(rows)
